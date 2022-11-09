7474871c1899acb15660361938c0983253e33078
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-09 16:25:38 +0100

    gg wp (easy)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,24 @@ const buttonEl = document.getElementById("button");
 buttonEl.addEventListener("click", function() {
     const options = document.getElementById("grids");
     let grid = parseInt(options.value);
+    console.log("Numero righe: " + grid);
 
-    grid = grid * grid;
-    console.log(grid);
+    let totGrid = grid * grid;
+    console.log("Numero celle: " + totGrid);
+
+    const gridContainerEl = document.querySelector(".grid-container");
+    gridContainerEl.innerHTML = "";
+
+    for (let i = 1; i <= totGrid; i++) {
+        const newCell = document.createElement("div");
+        newCell.classList.add("cell");
+        newCell.style.flexBasis = 100 / grid + "%";
+        newCell.innerHTML = `${i}`;
+        gridContainerEl.append(newCell);
+
+        newCell.addEventListener("click", function() {
+            this.classList.toggle("bg-primary");
+            console.log(this.textContent);
+        })
+    }
 })
